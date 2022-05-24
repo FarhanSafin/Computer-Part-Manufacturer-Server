049d05ca1ea08b998a345b3de8ea7dae70dc6765
Farhan <farhansafin@gmail.com>
2022-05-25 00:52:11 +0600

    delete, myorder api done

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ async function run () {
                     res.send(result);
                 })
 
+                //delete
+                app.delete('/part/:id', async(req, res) => {
+                    const id = req.params.id;
+                    const query = {_id: ObjectId(id)};
+                    const result = await orderCollection.deleteOne(query);
+                    res.send(result);
+                })
+
     }
 
     finally{
