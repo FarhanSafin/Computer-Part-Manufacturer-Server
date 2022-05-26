fb64d3c6e79e198be15043c6158e92194687ea6d
Farhan <farhansafin@gmail.com>
2022-05-26 18:40:04 +0600

    product status update added

--- index.js
+++ index.js
@@ -180,12 +180,7 @@ async function run () {
                 })
 
 
-                app.delete('/part/:id', async(req, res) => {
-                    const id = req.params.id;
-                    const query = {_id: ObjectId(id)};
-                    const result = await partsCollection.deleteOne(query);
-                    res.send(result);
-                })
+
 
 
 
@@ -232,18 +227,27 @@ async function run () {
                 })
 
 
-                app.put('/user/:email', async(req, res) => {
-                    const email = req.params.email;
-                    const user = req.body;
-                    const filter = {email: email};
-                    const options = {upsert: true};
-                    const updateDoc = {
-                        $set: user,
-                    };
-                    const result = await userCollection.updateOne(filter, updateDoc, options);
-                    const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
-                    res.send({result, token})
+
+
+                                app.delete('/part/:id', async(req, res) => {
+                    const id = req.params.id;
+                    const query = {_id: ObjectId(id)};
+                    const result = await partsCollection.deleteOne(query);
+                    res.send(result);
                 })
+
+
+                app.put('/updatestatus/:id', verifyJWT, async(req, res) => {
+                    const id = req.params.id;
+                    const filter = {_id: ObjectId(id)};
+                     const updateDoc = {
+                            $set: {paid: 'Shipped'},
+                        };
+                        const result = await orderCollection.updateOne(filter, updateDoc);
+                        res.send({result})
+                    })
+
+
 
 
                 app.put('/adduserinfo/:email', async(req, res) => {
@@ -256,6 +260,25 @@ async function run () {
                     };
                     const result = await userCollection.updateOne(filter, updateDoc, options);
                     res.send(result)
+                })
+
+
+
+
+
+
+
+                app.put('/user/:email', async(req, res) => {
+                    const email = req.params.email;
+                    const user = req.body;
+                    const filter = {email: email};
+                    const options = {upsert: true};
+                    const updateDoc = {
+                        $set: user,
+                    };
+                    const result = await userCollection.updateOne(filter, updateDoc, options);
+                    const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+                    res.send({result, token})
                 })
 
 
