539ba0ecc497a88b4900f43977e98b91141d2796
Farhan <farhansafin@gmail.com>
2022-05-26 19:26:56 +0600

    delete order added by admin

--- index.js
+++ index.js
@@ -181,6 +181,15 @@ async function run () {
 
 
 
+                app.delete('/customerorder/:id', async(req, res) => {
+                    const id = req.params.id;
+                    const query = {_id: ObjectId(id)};
+                    const result = await orderCollection.deleteOne(query);
+                    res.send(result);
+                })
+
+
+
 
 
 
