b84eace73315ccb4f970d5c3f18b36d03945faed
Farhan <farhansafin@gmail.com>
2022-05-26 16:35:59 +0600

    amount related bug fixed

--- index.js
+++ index.js
@@ -76,7 +76,8 @@ async function run () {
 
     
         app.post('/createpaymentintent', verifyJWT, async(req, res) => {
-            const {amount} = req.body;
+            let {price} = req.body;
+            const amount = price * 100;
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: 'usd',
